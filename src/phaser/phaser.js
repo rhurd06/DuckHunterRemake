ee6c849644e2ca9f4c2c4f94a4469efb49a4597b
johnturner4004 <johnturner4004@gmail.com>
2021-08-25 16:06:07 -0400

    change phaser logo to flying duck

--- src/phaser/phaser.js
+++ src/phaser/phaser.js
@@ -1,10 +1,11 @@
 import phaser from "phaser";
 import logo from "../assets/logo.png";
+import duck from '../assets/duckHuntBird.png'
 
 var config = {
   type: phaser.AUTO,
   parent: "phaser-example",
-  width: 800,
+  width: 1800,
   height: 600,
   scene: {
     preload: preload,
@@ -15,15 +16,15 @@ var config = {
 let game = new phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logo);
+  this.load.image("duck", duck);
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+  const logo = this.add.image(1000, 300, "duck");
 
   this.tweens.add({
     targets: logo,
-    y: 450,
+    x: 450,
     duration: 2000,
     ease: "Power2",
     yoyo: true,
