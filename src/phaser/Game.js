7ad8cf673921e19498bb196edb2248007214a182
Robyn Noble <robynhurd09@gmail.com>
2021-08-27 12:20:38 -0500

    fighting with phaser to get background and sprite to show up on the browser

--- src/phaser/Game.js
+++ src/phaser/Game.js
@@ -1,19 +1,20 @@
-import phaser from 'phaser';
+// import phaser from 'phaser';
 
-export default class Game extends phaser.Scene{
-  constructor() {
-    super('game');
-  }
+// export default class Game extends phaser.Scene{
+//   constructor() {
+//     super('game');
+//   }
 
-  preload() {
-    this.load.image('sky', './assets/sky.jpg');
-    this.load.spritesheet('duck', './assets/duckspritesheet.png', 
-        {frameWidth: 100, frameHeight: 100});
-  }
+//   preload() {
+//     this.load.image('sky', './assets/sky.jpg');
+//     this.load.spritesheet('duck', './assets/duckspritesheet.png', 
+//         {frameWidth: 500, frameHeight: 700});
+//   }
 
-  create() {
-    this.add.image(0, 0, 'sky').setOrigin(0, 0);
-  }
+//   create() {
+//     this.add.image(500, 700, 'sky').setOrigin(0, 0);
+//     gameState.duck = this.add.sprite(100, 100, 'duck')
+//   }
 
 
-}
+// }
--- src/phaser/phaser.js
+++ src/phaser/phaser.js
@@ -1,17 +1,41 @@
 import phaser from "phaser";
 import logo from "../assets/logo.png";
-import Game from './Game'
+import sky2 from '../assets/sky2.jpeg';
+import duckHuntBird from '../assets/duckHuntBird.png';
+
+  // constructor() {
+  //   super('scene')
+  // };
+
+  function preload() {
+    this.load.image('sky', sky2);
+    this.load.spritesheet('duck', '../assets/duckspritesheet.png',
+      { frameWidth: 500, frameHeight: 700 });
+    this.load.image('duck2', duckHuntBird)
+  }
+
+  function create() {
+    this.add.image(500, 700, 'sky').setOrigin(0, 0);
+    this.add.sprite(100, 100, 'duck');
+    this.add.sprite(200, 100, 'duck2').setScale(.2);
+  }
+
 
 var config = {
   type: phaser.AUTO,
-  parent: "phaser-example",
   backgroundColor: "#8FD9F6",
   width: 800,
   height: 600,
-  scene: [Game]
+  scene: {
+    preload,
+    create
+  }
 };
 
-let game = new phaser.Game(config);
+
+
+const game = new phaser.Game(config);
+
 
 // function preload() {
 //   this.load.image("logo", logo);
