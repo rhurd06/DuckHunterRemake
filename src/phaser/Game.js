619e6246bdd2c0c6c7d0eea4d0e7e4aea6c8c4d6
Robyn Noble <robynhurd09@gmail.com>
2021-08-27 11:10:23 -0500

    starting to set up scene

---
+++ src/phaser/Game.js
@@ -0,0 +1,19 @@
+import phaser from 'phaser';
+
+export default class Game extends phaser.Scene{
+  constructor() {
+    super('game');
+  }
+
+  preload() {
+    this.load.image('sky', './assets/sky.jpg');
+    this.load.spritesheet('duck', './assets/duckspritesheet.png', 
+        {frameWidth: 100, frameHeight: 100});
+  }
+
+  create() {
+    this.add.image(0, 0, 'sky')
+  }
+
+
+}
--- src/phaser/phaser.js
+++ src/phaser/phaser.js
@@ -1,34 +1,33 @@
 import phaser from "phaser";
 import logo from "../assets/logo.png";
+import Game from './Game'
 
 var config = {
   type: phaser.AUTO,
   parent: "phaser-example",
+  backgroundColor: "#8FD9F6",
   width: 800,
   height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
+  scene: [Game]
 };
 
 let game = new phaser.Game(config);
 
-function preload() {
-  this.load.image("logo", logo);
-}
+// function preload() {
+//   this.load.image("logo", logo);
+// }
 
-function create() {
-  const logo = this.add.image(400, 150, "logo");
+// function create() {
+//   const logo = this.add.image(400, 150, "logo");
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
+//   this.tweens.add({
+//     targets: logo,
+//     y: 450,
+//     duration: 2000,
+//     ease: "Power2",
+//     yoyo: true,
+//     loop: -1
+//   });
+// }
 
 export { game };
